9b1e87cd0882ffe4870e87d3b98193d8a67658cd
Muhammad Tanveer <tanveerofficial244@gmail.com>
2024-12-13 10:38:11 +0500

    Api gate added in microservices ocelotGateway

---
+++ OcelotGateway/Program.cs
@@ -0,0 +1,39 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Logging.AddConsole();
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.Listen(IPAddress.Any, 5000);  // Or any other free port
+//});
+// Add Ocelot configuration
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+// Add Ocelot services
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+// Configure Ocelot middleware
+app.UseOcelot().Wait();
+
+app.Run();
