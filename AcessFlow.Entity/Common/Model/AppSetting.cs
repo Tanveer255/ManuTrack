9fd2f8f0153db2d71bdc58ce97c83c5fd457ebb6
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-02-17 23:49:05 +0500

    Send Grid model added

---
+++ AcessFlow.Entity/Common/Model/AppSetting.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessFlow.Entity.Common.Model;
+
+public class AppSetting
+{
+    public static string ConnectionString { get; set; }
+
+    public static string ReCaptchaSiteKey { get; set; }
+
+    public static string ReCaptchaSecretKey { get; set; }
+
+    public static string ReCaptchaUrl { get; set; }
+
+    public static string SendGridEmailApiKey { get; set; }
+
+    public static string SendGridValidationApiKey { get; set; }
+
+    public static string SendGridFromEmail { get; set; }
+
+    public static string ApplicationEmailValidationEnabled { get; set; }
+}
