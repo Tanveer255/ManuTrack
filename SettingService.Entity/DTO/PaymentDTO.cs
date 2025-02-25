a5a23658f641a7e62d204d67628e295e14c93ca3
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-02-25 23:28:16 +0500

    paymentDTOS Added

---
+++ SettingService.Entity/DTO/PaymentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingService.Entity.DTO;
+
+public class PaymentDTO
+{
+    public string PaymentMethodId { get; set; }
+    public string CustommerId { get; set; }
+}
---
+++ SettingService.Entity/DTO/StripePaymentRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingService.Entity.DTO;
+
+public class StripePaymentRequestDTO
+{
+    public string Email { get; set; }
+    public string PaymentMethodId { get; set; }
+}
