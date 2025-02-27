7971638bb6f147951e429f0b689e3b335367bb52
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-02-27 22:20:24 +0500

    Stripe web hook implement

--- AcessFlow/Controllers/WebhookController.cs
+++ AcessFlow/Controllers/WebhookController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,25 +9,39 @@ namespace AcessFlow.Controllers;
 [ApiController]
 public class WebhookController : ControllerBase
 {
-    //[HttpPost("stripe")]
-    //public async Task<IActionResult> StripeWebhook()
-    //{
-    //    var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-    //    var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], "whsec_...");
-    //    switch (stripeEvent.Type)
-    //    {
-    //        case "checkout.session.completed":
-    //            var session = stripeEvent.Data.Object as Session;
-    //            // Fulfill the purchase...
-    //            break;
-    //        case "payment_intent.succeeded":
-    //            var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
-    //            // Fulfill the purchase...
-    //            break;
-    //        default:
-    //            // Handle other event types...
-    //            break;
-    //    }
-    //    return Ok();
-    //}
+    private const string endpointSecret = "your_webhook_secret";
+
+    [HttpPost]
+    public async Task<IActionResult> HandleWebhook()
+    {
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+        try
+        {
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], endpointSecret);
+
+            // Ensure correct event names from Stripe
+            if (stripeEvent.Type == "customer.subscription.created")
+            {
+                var subscription = stripeEvent.Data.Object as Subscription;
+                // Handle subscription created event
+            }
+            else if (stripeEvent.Type == "customer.subscription.updated")
+            {
+                var subscription = stripeEvent.Data.Object as Subscription;
+                // Handle subscription updated event
+            }
+            else if (stripeEvent.Type == "customer.subscription.deleted")
+            {
+                var subscription = stripeEvent.Data.Object as Subscription;
+                // Handle subscription cancellation
+            }
+
+            return Ok();
+        }
+        catch (StripeException e)
+        {
+            return BadRequest(new { error = e.Message });
+        }
+    }
 }
