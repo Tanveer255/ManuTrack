c90366de44b4c12702f7c609f78ead1a120d10ee
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-03-17 23:25:27 +0500

    added authentication

--- JwtAuthentication/Service/JwtAuthenticationService.cs
+++ JwtAuthentication/Service/JwtAuthenticationService.cs
@@ -1,36 +1,35 @@
-﻿namespace JwtAuthentication.Service
+﻿namespace JwtAuthentication.Service;
+
+public class JwtAuthenticationService : IJwtAuthenticationService
 {
-    public class JwtAuthenticationService : IJwtAuthenticationService
+    private readonly JwtSettings _jwtSettings;
+    private readonly IConfiguration _configuration;
+
+    public JwtAuthenticationService(IOptions<JwtSettings> jwtSettings, IConfiguration configuration)
     {
-        private readonly JwtSettings _jwtSettings;
-        private readonly IConfiguration _configuration;
+        _jwtSettings = jwtSettings.Value;
+        _configuration = configuration;
+    }
 
-        public JwtAuthenticationService(IOptions<JwtSettings> jwtSettings, IConfiguration configuration)
+    public string GenerateJwtToken(string username)
+    {
+        var claims = new[]
         {
-            _jwtSettings = jwtSettings.Value;
-            _configuration = configuration;
-        }
+        new Claim(ClaimTypes.Name, username),
+        // Add more claims if needed
+    };
 
-        public string GenerateJwtToken(string username)
-        {
-            var claims = new[]
-            {
-            new Claim(ClaimTypes.Name, username),
-            // Add more claims if needed
-        };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+            signingCredentials: credentials
+        );
 
-            var token = new JwtSecurityToken(
-                issuer: _jwtSettings.Issuer,
-                audience: _jwtSettings.Audience,
-                claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
-                signingCredentials: credentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
