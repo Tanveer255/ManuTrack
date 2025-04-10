9c5eb896b4ab5ed880faba404ad730127b4feb15
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-04-11 00:18:05 +0500

    CommonUtils class added in Infrastrucure for common function for all services

---
+++ JwtAuthentication/Model/Common/CommonUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Model.Common;
+
+public static class CommonUtils
+{
+    public static bool IsValidEmail(string email)
+    {
+        return !string.IsNullOrWhiteSpace(email) &&
+               System.Text.RegularExpressions.Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+    }
+
+    public static string GenerateSlug(string input)
+    {
+        return input?.ToLower()
+                     .Replace(" ", "-")
+                     .Replace(".", "")
+                     .Replace(",", "")
+                     .Trim();
+    }
+
+    public static string GetFormattedDate(DateTime date)
+    {
+        return date.ToString("yyyy-MM-dd");
+    }
+}
